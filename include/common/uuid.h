b744d4fef9c9b85c30b67380a2ea6cf7aa7869c6
Marc Bonnici <marc.bonnici@arm.com>
2021-08-12 16:08:27 +0100

    uuid: Rename include guard to be unique
    There is other separate UUID functionality defined elsewhere
    with the same guard therefore rename this to be unique and enable
    it to be utilised within the SPMC.
    
    Change-Id: I89c6b99b9791417f5d65c31bbc94be38cfbdac89
    Signed-off-by: Marc Bonnici <marc.bonnici@arm.com>

--- include/common/uuid.h
+++ include/common/uuid.h
@@ -4,12 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef UUID_H
-#define UUID_H
+#ifndef COMMON_UUID_H
+#define COMMON_UUID_H
 
 #define UUID_BYTES_LENGTH	16
 #define UUID_STRING_LENGTH	36
 
 int read_uuid(uint8_t *dest, char *uuid);
 
-#endif /* UUID_H */
+#endif /* COMMON_UUID_H */
